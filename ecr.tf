9bbe34f49309341269681c6f78ed20534c625004
Eli Fuchsman <efuchsman@gmail.com>
2024-03-05 17:41:30 -0700

    Build terraform config to push image

--- ecr.tf
+++ ecr.tf
@@ -1,11 +1,35 @@
-#ecr.tf
+# ecr.tf
 
 terraform {
   required_providers {
-    aws = ">= 5.36.0, <= 5.36.0"
+    aws = ">= 3.0, < 4.0"
   }
 }
 
 provider "aws" {
-  region = var.AWS_REGION
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+data "aws_ecr_repository" "existing_repository" {
+  name = var.ECR_REPOSITORY
+}
+
+resource "null_resource" "build_and_push" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker build -t ${data.aws_ecr_repository.existing_repository.repository_url}:elifuchsmanbe-app .
+      docker login -u AWS -p $(aws ecr get-login-password --region us-west-2) ${data.aws_ecr_repository.existing_repository.repository_url}
+      docker push ${data.aws_ecr_repository.existing_repository.repository_url}:elifuchsmanbe-app
+    EOT
+  }
+}
+
+output "docker_image_url" {
+  value = "${data.aws_ecr_repository.existing_repository.repository_url}:elifuchsmanbe-app"
 }
